e2d85f32c666587a2c097cf4236e630b8d191277
SK ISMILE <101392872+skismile@users.noreply.github.com>
2022-12-14 21:32:05 +0530

    testing backend

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/user",UserRouter)
 app.get('/',(req,res)=>{res.send('hello')})
 mongoose.set("strictQuery", false)
 
-mongoose.connect("mongodb+srv://skismile:7867@cluster0.ngknmv4.mongodb.net/?retryWrites=true&w=majority",()=>{
+mongoose.connect("mongodb+srv://skismile:7867@cluster0.mlcz95a.mongodb.net/test",()=>{
 
     app.listen(8081,()=>{console.log('server is runing on port 8081')})
 })
