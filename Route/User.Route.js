de0ba2bb33cb2cd809b2d9c96d08c1f2ce222de2
SK ISMILE <101392872+skismile@users.noreply.github.com>
2022-12-14 19:20:09 +0530

    testing backend

---
+++ Route/User.Route.js
@@ -0,0 +1,33 @@
+const express=require("express")
+const app=express.Router()
+const UserModel=require("../Model/User.Model")
+
+app.get("/",(req,res)=>{
+
+
+res.send("user")
+
+
+})
+
+
+app.post("/create",async(req,res)=>{
+let {email,password,name}=req.body
+
+console.log(email,password,name)
+
+try{
+console.log(UserModel)
+    let user = await UserModel.create({email,password,name})
+    
+    res.send("user created")
+}catch(e){
+    res.send("user not created")
+    // console.log(e)
+}
+    
+    
+    })
+
+
+module.exports=app
---
+++ server.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const mongoose=require("mongoose")
+const app=express()
+const UserRouter=require("./Route/User.Route")
+const cors=require("cors")
+app.use(express.urlencoded({extended : true}))
+
+app.use(express.json())
+app.use(cors())
+app.use("/user",UserRouter)
+app.get('/',(req,res)=>{res.send('hello')})
+mongoose.set("strictQuery", false)
+
+mongoose.connect("mongodb+srv://skismile:7867@cluster0.mfvci43.mongodb.net/testingdb",()=>{
+
+    app.listen(8081,()=>{console.log('server is runing on port 8080')})
+})
