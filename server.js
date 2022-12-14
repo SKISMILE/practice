9ea73be03a4daaafb5731f88742bb8e74a6b96b9
SK ISMILE <101392872+skismile@users.noreply.github.com>
2022-12-14 21:35:03 +0530

    testing backend

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express=require('express')
+require("dotenv").config()
 const mongoose=require("mongoose")
 const app=express()
 const UserRouter=require("./Route/User.Route")
 const cors=require("cors")
+const PORT=process.env.PORT ||8081
+const MONGO_URL=process.env.MONGO_URL
 app.use(express.urlencoded({extended : true}))
 
 app.use(express.json())
@@ -11,7 +14,7 @@ app.use("/user",UserRouter)
 app.get('/',(req,res)=>{res.send('hello')})
 mongoose.set("strictQuery", false)
 
-mongoose.connect("mongodb+srv://skismile:7867@cluster0.mlcz95a.mongodb.net/test",()=>{
+mongoose.connect(MONGO_URL,()=>{
 
-    app.listen(8081,()=>{console.log('server is runing on port 8081')})
+    app.listen(PORT,()=>{console.log('server is runing on port 8081')})
 })
